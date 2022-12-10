b9a76b3f225cd2fc02e7c97e439ede29f679c404
MustaGitRefresh <kabcoco.2gitdom.3@gmail.com>
2022-12-11 00:17:50 +0530

    Revert "remove the ones function by reverting"
    This reverts commit 4b7812919b17153723b1dfe2608d1bee9dfe9295.

--- ProjectSetUper/src/project_setup.py
+++ ProjectSetUper/src/project_setup.py
@@ -18,7 +18,6 @@ class Dir:
         self.createDir()
         self.error_output_list(self.output_list, self.error_list)
         self.sub_main_folder()
-        self.src_tests()
 
     def createDir(self):
         os.chdir(self.path)
@@ -33,19 +32,14 @@ class Dir:
         print(os.getcwd())
         self.git_repo_init()
 
-    def os_ones(self, sub_folder_path, folder_name):
-        if not folder_name:
-            folder_name = self.project_name
+    def sub_main_folder(self):
+        sub_folder_path = self.path + "\\{}".format(self.project_name)
         os.chdir(sub_folder_path)
         print("change sub")
         print(os.getcwd())
-        os.mkdir(folder_name)
-        os.path.join(sub_folder_path, folder_name)
+        os.mkdir(self.project_name)
+        os.path.join(sub_folder_path, self.project_name)
         print(os.getcwd())
-
-    def sub_main_folder(self):
-        sub_folder_path = self.path + "\\{}".format(self.project_name)
-        self.os_ones(sub_folder_path, None)
 
     def git_repo_init(self):
         out_list, err_list = Git().subprocess_git()
@@ -62,12 +56,6 @@ class Dir:
     def taking_project_name_user(self):
         project_name = input("Enter the project name:\n")
         self.project_name = project_name
-
-    def src_tests(self):
-        folders = ['src', 'tests']
-        for i in range(len(folders)):
-            src_tests = self.path + '\\{}'.format(folders[i])
-            self.os_ones(src_tests, folders[i])
 
 
 git_directory = Dir()
