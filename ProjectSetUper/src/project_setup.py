6f3ecce89373367ca2197b0437e07380f316baeb
MustaGitRefresh <kabcoco.2gitdom.3@gmail.com>
2022-12-11 00:23:37 +0530

    again reverting to dynamic project names.

--- ProjectSetUper/src/project_setup.py
+++ ProjectSetUper/src/project_setup.py
@@ -15,6 +15,7 @@ class Dir:
         self.createDir()
         self.error_output_list(self.output_list, self.error_list)
         self.sub_main_folder()
+        self.src_tests()
 
     def createDir(self):
         path = "E:\\Pyton Projects"
@@ -30,6 +31,7 @@ class Dir:
         print(os.getcwd())
         self.git_repo_init()
 
+<<<<<<< HEAD
     def sub_main_folder(self):
         path = 'E:\\Pyton Projects\\GitAuto'
         os.chdir(path)
@@ -39,6 +41,22 @@ class Dir:
         os.path.join(path, 'GitAuto')
         print(os.getcwd())
     print()
+=======
+    def os_ones(self, sub_folder_path, folder_name):
+        if not folder_name:
+            folder_name = self.project_name
+        os.chdir(sub_folder_path)
+        print("change sub")
+        print(os.getcwd())
+        os.mkdir(folder_name)
+        os.path.join(sub_folder_path, folder_name)
+        print(os.getcwd())
+
+    def sub_main_folder(self):
+        sub_folder_path = self.path + "\\{}".format(self.project_name)
+        self.os_ones(sub_folder_path, None)
+
+>>>>>>> parent of b9a76b3 (Revert "remove the ones function by reverting")
     def git_repo_init(self):
         out_list, err_list = Git().subprocess_git()
         self.output_list = out_list
@@ -51,5 +69,18 @@ class Dir:
         print("_____________________________________________________")
         print(err)
 
+<<<<<<< HEAD
+=======
+    def taking_project_name_user(self):
+        project_name = input("Enter the project name:\n")
+        self.project_name = project_name
+
+    def src_tests(self):
+        folders = ['src', 'tests']
+        for i in range(len(folders)):
+            src_tests = self.path + '\\{}'.format(folders[i])
+            self.os_ones(src_tests, folders[i])
+
+>>>>>>> parent of b9a76b3 (Revert "remove the ones function by reverting")
 
 git_directory = Dir()
