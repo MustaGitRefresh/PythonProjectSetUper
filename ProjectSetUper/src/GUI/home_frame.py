6200edc697108f60adb711409b6cd47832b18b58
MustaGitRefresh <kabcoco.2gitdom.3@gmail.com>
2023-02-21 10:23:28 +0530

    Simple GUI is created but some modifications has to be done for its proper functionality.

--- ProjectSetUper/src/GUI/home_frame.py
+++ ProjectSetUper/src/GUI/home_frame.py
@@ -2,7 +2,8 @@
 
 # Imports
 from tkinter import Tk
-from tkinter import Label
+from tkinter import Label, Frame, Entry
+from tkinter import RIDGE
 
 
 # Creating the Base class for the GUI app
@@ -14,7 +15,16 @@ class Home:
         self.set_size(height=500, width=500)
 
         # Components
-        self.label_app()
+        self.label_title()
+        # Frame
+        self.frame = self.inputFrame()
+        self.frame.pack()
+        # Entry label
+        self.label_entry(text="Enter the path", padding=[50, 50])
+        user_entry_path = self.entryFields()
+        self.label_entry(text='Enter the project name', padding=[50, 50])
+        user_entry_project_name = self.entryFields()
+        # Entry field
 
         self.root.mainloop()
 
@@ -26,11 +36,42 @@ class Home:
         y = (screen_height // 2) - (height // 2)
 
         self.root.geometry(f'{height}x{width}+{x}+{y}')
-        self.root.maxsize(600, 600)
+        self.root.resizable(False, False)
 
-    def label_app(self):
-        label = Label(self.root, text="ProjectSetUpper")
-        label.pack()
+    def label_title(self):
+        font = "Helvetica 25 bold"
+        label = Label(self.root, text="Project Set Upper", font=font,
+                      fg='#324ea8',
+                      bg='#42a832',
+                      borderwidth=1,
+                      width=100,
+                      relief=RIDGE)
+        label.pack(pady=50)
+
+    def label_entry(self, text, padding: list, font="Helvetica 15 bold"):
+        entry_label = Label(
+            self.frame,
+            text=text,
+            font=font,
+        )
+        entry_label.pack(
+            pady=padding[0],
+            padx=padding[1]
+        )
+
+    def inputFrame(self):
+        entry_frame = Frame(self.root)
+        return entry_frame
+
+    def entryFields(self, font="Helvetica 20 bold"):
+        user_entry = Entry(
+            self.frame,
+            font=font
+        )
+
+        user_entry.pack()
+
+        return user_entry
 
 
 root = Tk()
